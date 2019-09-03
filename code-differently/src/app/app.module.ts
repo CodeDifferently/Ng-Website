f8dd43435ac9523e96c7d2e3f84c962c574878b6
Nazhir Jackson <naz@codedifferently.com>
2019-09-02 22:35:24 -0400

    finish nav and header css

--- code-differently/src/app/app.module.ts
+++ code-differently/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NavComponent } from './layout/nav/nav.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { ContentLayoutComponent }  from './layout/content-layout/content-layout.component';
+import { NavComponent } from './core/layout/nav/nav.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
+import { ContentLayoutComponent }  from './core/layout/content-layout/content-layout.component';
 
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
