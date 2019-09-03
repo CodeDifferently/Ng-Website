4e99658d1a9668c00a3649dfcbf55ecfe93fe043
Nazhir Jackson <naz@codedifferently.com>
2019-09-03 03:23:57 -0400

    about page prototyped

--- code-differently/src/app/app-routing.module.ts
+++ code-differently/src/app/app-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
     {
       path: 'home',
       loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+    },
+    {
+      path: 'about',
+      loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule)
     }
   ]
   }
