9e2e9e2e39085d2822b078e897ac3ea967a75eba
Nazhir Jackson <naz@codedifferently.com>
2019-09-12 11:16:43 -0400

    dynamic header

--- code-differently/src/app/core/layout/header/header.component.ts
+++ code-differently/src/app/core/layout/header/header.component.ts
@@ -2,8 +2,12 @@ import { Component} from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
 let url: string;
-
-
+let bg_copy: string;
+const routes = [
+  {route: '/home', copy: 'Introduce and Elevate Our Youth'},
+  {route: '/about', copy:  'Some About Us copy'},
+  {route: 'program', copy: ''}
+]
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -14,14 +18,24 @@ export class HeaderComponent {
   constructor(private router: Router) {
     this.router.events.subscribe((ev) => {
       if(ev instanceof NavigationEnd){
-         url = this.router.url;
-        //  document.getElementById('cover').style.background = " #2b2d34 url('../../../../assets/images/"+url+".jpg') center";
-        document.getElementById('slogan').innerHTML = url; 
-        
-        console.log(url);
+        url = this.router.url;
+        bg_copy = this.checkRoute(url);
+        document.getElementById('cover-container').style.background = " #2b2d34 url('../../../../assets/images"+url+"_banner.png') center";
+        document.getElementById('slogan').innerHTML = bg_copy; 
+
       }
     })
 
+  }
+
+  private checkRoute(url: string): string {
+    for(let i = 0; i < routes.length - 1; i++){
+      if(routes[i].copy === url) {
+        return routes[i].copy;
+      }
+    }
+
+    return routes[0].copy;
   }
 
 
